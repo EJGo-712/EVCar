33ab5c2917d227bb48761ce9236ce57d9caf2c9d
EunJong <eunha-o712@gmail.com>
2026-03-26 15:20:17 +0900

    feat. main 1차 시안

--- src/main/java/com/evcar/controller/MainController.java
+++ src/main/java/com/evcar/controller/MainController.java
@@ -1,13 +1,22 @@
 package com.evcar.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/main")
 public class MainController {
-	@RequestMapping("/")
-	public String main() {
-		return "layout/header";
-		
-	}	
+
+    // /main , /main/ 둘 다 처리
+    @GetMapping({"", "/"})
+    public String main() {
+        return "main/index";
+    }
+
+    // /main/index
+    @GetMapping("/index")
+    public String index() {
+        return "main/index";
+    }
 }
