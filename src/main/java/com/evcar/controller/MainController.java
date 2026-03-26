9fadf8845b11737b6449c5796457e1e72ff5742b
EunJong <eunha-o712@gmail.com>
2026-03-26 09:46:49 +0900

    feat. gpt 만드느라 바빴음

---
+++ src/main/java/com/evcar/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.evcar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("/")
+	public String main() {
+		return "layout/header";
+		
+	}	
+}
