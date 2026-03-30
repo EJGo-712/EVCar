7ee89f5bb8a83979a22ca068b2648e7b3a1004c3
min <minkiy76@gmail.com>
2026-03-30 10:01:43 +0900

    Rename EvcarApplication to EvCarApplication

--- src/main/java/com/evcar/EvcarApplication.java
+++ src/main/java/com/evcar/EvcarApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EvcarApplication{
+public class EvCarApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(EvcarApplication.class, args);
 	}
